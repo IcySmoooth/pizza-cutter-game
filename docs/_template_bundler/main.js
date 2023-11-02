bb73340da833d5a7e32fa3e6c61a1bfa9d78b89e
IcySmoooth <90065902+IcySmoooth@users.noreply.github.com>
2023-11-02 15:12:51 -0700

    Initial commit

---
+++ docs/_template_bundler/main.js
@@ -0,0 +1,21 @@
+import "crisp-game-lib";
+
+const title = "";
+
+const description = `
+`;
+
+const characters = [];
+
+function update() {
+  if (!ticks) {
+  }
+}
+
+init({
+  update,
+  title,
+  description,
+  characters,
+  options: {},
+});
---
+++ docs/pizzacutter/main.js
@@ -0,0 +1,13 @@
+title = "Pizza Cutter";
+
+description = `
+`;
+
+characters = [];
+
+options = {};
+
+function update() {
+  if (!ticks) {
+  }
+}
