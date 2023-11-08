690d3b9b41d13c8bbfe792c30097ae2e163fb69f
unknown <rubytseng.usa@gmail.com>
2023-11-07 16:27:41 -0800

    Added pizza base

--- docs/pizzacutter/main.js
+++ docs/pizzacutter/main.js
@@ -7,7 +7,29 @@ characters = [];
 
 options = {};
 
+/**
+*@typedef {{
+  *	 pos: Vector
+  * }} Pizza
+  */
+    
+  /**
+  * @type { Pizza [] }
+  */
+  let pizza;
+
 function update() {
   if (!ticks) {
+    pizza = {
+			pos: vec(50, 75)
+		};
   }
+
+  color("light_yellow");
+	arc(pizza.pos, 10, 50, 0, 3);
+	arc(pizza.pos, 10, 50, 0, -3.2);
+
+	color("yellow");
+	arc(pizza.pos, 40, 10, 0, 3);
+	arc(pizza.pos, 40, 10, 0, -3.2);
 }
