94b2256f07419812b5ab4dc4b943349891099c0a
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-11-08 13:17:55 -0800

    O - col remove unused code

--- docs/pizzacutter/main.js
+++ docs/pizzacutter/main.js
@@ -75,29 +75,6 @@ function generatePizza(pizza, slices) {
    */
   let lightColor = "light_yellow";
 
-  
-  /*for (let i = -1; i < slices.length; i++) {
-    let angle1;
-    if (i == -1) {
-      angle1 = -PI;
-    } else {
-      angle1 = slices[i];
-    }
-    
-    let angle2;
-    if (i == slices.length - 1) {
-      angle2 = PI;
-    } else {
-      angle2 = slices[i + 1];
-    }
-
-    color(lightColor);
-    arc(50, 50, 5, 50, angle1, angle2);
-
-    color(colors);
-    arc(50, 50, 30, 10, angle1, angle2);
-  }*/
-
   color(lightColor);
   arc(pizza.pos, 10, 50, -PI, PI);
 
@@ -145,4 +122,3 @@ function getPoint(radius, angle) {
 
   return vec(x, y);
 }
-
