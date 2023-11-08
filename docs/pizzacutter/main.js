663339246e39783016978505c4bba57057a1fb2d
root <root@LAPTOP-APMQHGC4>
2023-11-08 13:45:23 -0800

    added the cutter and pepperoni

--- docs/pizzacutter/main.js
+++ docs/pizzacutter/main.js
@@ -3,7 +3,22 @@ title = "Pizza Cutter";
 description = `
 `;
 
-characters = [];
+characters = [
+  `
+  l
+  l
+ lll
+ lll
+lllll
+lllll
+  `,
+  `
+ ll
+l  l
+l  l
+ ll
+  `
+];
 
 options = {};
 
@@ -27,28 +42,60 @@ options = {};
   * @type { Pepperoni [] }
   */
   let pepperoni;
+  /**
+*@typedef {{
+  *	 pos: Vector
+  * }} Olive
+  */
+  
+  /**
+  * @type { Olive [] }
+  */
+  let olive;
+  
+  /**
+ * @typedef {{
+ * pos: Vector,
+  * }} Cutter
+  */
+ 
+ /**
+  * @type { Cutter }
+  */
+ let cutter;
   
 
 function update() {
   if (!ticks) {
     pizza = {
-			pos: vec(50, 75)
+			pos: vec(50, 55)
 		};
     pepperoni = {
-			pos: vec(60, 60)
+			pos: vec(60, 40)
 		};
+    olive = {
+			pos: vec(40, 50)
+		};
+    cutter = {
+      pos: vec(50, 95)
+  };
   }
 
   color("light_yellow");
-	arc(pizza.pos, 10, 50, 0, 3);
-	arc(pizza.pos, 10, 50, 0, -3.2);
+	arc(pizza.pos, 10, 33, 0, 3);
+	arc(pizza.pos, 10, 33, 0, -3.2);
 
 	color("yellow");
-	arc(pizza.pos, 40, 10, 0, 3);
-	arc(pizza.pos, 40, 10, 0, -3.2);
+	arc(pizza.pos, 30, 7, 0, 3);
+	arc(pizza.pos, 30, 7, 0, -3.2);
 
   color("red");
-	char("a", pepperoni.pos);
-	arc(pepperoni.pos, 4, 5, 0, 3);
-	arc(pepperoni.pos, 4, 5, 0, -3.2);
+	arc(pepperoni.pos, 3, 4.5, 0, 3);
+	arc(pepperoni.pos, 3, 4.5, 0, -3.2);
+
+  color("black");
+	char("b", olive.pos);
+
+  color("light_black");
+  char("a", cutter.pos);
 }
